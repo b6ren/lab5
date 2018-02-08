a21f58b441adf8fc97703b8e11938b742d496a5c
Boya Ren <b6ren@ucsd.edu>
2018-02-08 10:46:15 -0800

    m

--- routes/add.js
+++ routes/add.js
@@ -3,15 +3,9 @@ var data = require("../data.json");
 exports.addFriend = function(req, res) {    
 	var name = req.query.name;
 	var description = req.query.description;
-	{
-		"data": [
-            {
-        	    "name" : name,
-        	    "description" : description,
-        	    "imageURL": "http://lorempixel.com/400/400/people"
-            }
-		]
-	}
+	
+    var newData = {"name" : name,"description" : description,"imageURL": "http://lorempixel.com/400/400/people" };
+    data.friends.push(newData);
+	
 	res.render('index', data);
-		console.log("Friend is" + data);
  }
